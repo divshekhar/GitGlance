2601e268079d39d59cd9baff414b28eee37876cf
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-01-07 15:09:32 +0530

    show modal on glance

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Modal from "../modal/Modal";
+
 console.log("[GitGlance]: Content script loaded!");
 
 // listen for message from the background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // create alert
-  alert(request.url);
+  // Open Modal
+  const root = createRoot(document.body);
+  root.render(<Modal />);
 
   // send response to background script
   sendResponse("Hello from the content script!");
---
+++ src/modal/Modal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Modal.css";
+
+export default function Modal() {
+  return (
+    <div id="myModal" className="modal">
+      <div className="modal-content">
+        <span className="close">&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>
+    </div>
+  );
+}
