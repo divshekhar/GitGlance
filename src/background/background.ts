fcedc60854f20fbc5f19ed75a42b318d283c1c3d
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-01-07 13:29:42 +0530

    show context menu on blob & tree url patterns

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,17 +1,18 @@
-// Add Context Menu when extension is installed
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 
-  // create context menu
   const contextMenu: chrome.contextMenus.CreateProperties = {
     id: "glance",
     title: "Glance",
-    contexts: ["link"]
+    contexts: ["link"],
+    targetUrlPatterns: ["*://github.com/*/blob/*", "*://github.com/*/tree/*"]
   };
 
   // Add context menu
   chrome.contextMenus.create(contextMenu);
   chrome.contextMenus.onClicked.addListener((info, tab) => clickHandler(info, tab));
+
+  console.log("Context menu added");
 });
 
 function clickHandler(info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) {
