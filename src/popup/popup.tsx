877ab2735fe401cc74a05bc32222bb1884b13da5
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-01-07 19:10:18 +0530

    feat: store & get storage value

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,10 +4,18 @@ import Switch from "react-switch";
 import "./popup.css";
 
 export default function Popup() {
-  const [checked, setChecked] = React.useState(false);
+  const [checked, setChecked] = React.useState(true);
 
-  function toggleSwitch() {
+  // get the value from chrome storage
+  chrome.storage.sync.get(["on"]).then((result) => {
+    setChecked(result.on);
+  });
+
+  async function toggleSwitch() {
     setChecked(!checked);
+
+    // store the value in chrome storage
+    await chrome.storage.sync.set({ on: !checked });
   }
 
   return (
