762b8767831c173732e0d7cf55f975291f49499a
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-01-10 17:07:02 +0530

    file content and code block

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -7,8 +7,9 @@ console.log("[GitGlance]: Content script loaded!");
 // listen for message from the background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // Open Modal
-  const root = createRoot(document.body);
-  root.render(<Modal />);
+  const element = document.getElementById("repo-content-turbo-frame");
+  const root = createRoot(element ?? document.body);
+  root.render(<Modal url={request.url} />);
 
   // send response to background script
   sendResponse("Hello from the content script!");
--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CopyBlock, dracula } from "react-code-blocks";
 import "./Modal.css";
 
-export default function Modal() {
+interface Props {
+  url: string;
+}
+
+export default function Modal(props: Props) {
+  // content state
+  const [content, setContent] = useState("");
+  const [fetching, setFetching] = useState(false);
+
+  // get url from props
+  let url = props.url;
+
+  // replace github.com with raw.githubusercontent.com
+  url = url.replace("github.com", "raw.githubusercontent.com");
+
+  // remove /blob/ from url
+  url = url.replace("/blob/", "/");
+
+  // get content from the url
+  useEffect(() => {
+    const fetchContent = async () => {
+      setFetching(true);
+      const response = await fetch(url);
+      setContent(await response.text());
+      setFetching(false);
+    };
+
+    fetchContent();
+  }, []);
+
   return (
     <div id="myModal" className="modal">
       <div className="modal-content">
         <span className="close">&times;</span>
-        <p>Some text in the Modal..</p>
+        <p>GitGlance</p>
+        <p>Url: {props.url}</p>
+        <p>Raw Url: {url}</p>
+        {fetching && <p>Fetching Content...</p>}
+        {!fetching && <CopyBlock text={content} showLineNumbers={true} theme={dracula} wrapLines />}
       </div>
     </div>
   );
